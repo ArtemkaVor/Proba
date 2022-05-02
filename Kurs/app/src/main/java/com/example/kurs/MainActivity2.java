a03d2bc4caeb613c60d083570374390aaa1ef3c3
ArtemkaVor <barnkovich@mail.ru>
2022-05-02 15:09:46 +0700

    dfd

---
+++ Kurs/app/src/main/java/com/example/kurs/MainActivity2.java
@@ -0,0 +1,40 @@
+package com.example.kurs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ImageView gotomenu = (ImageView) findViewById(R.id.menugo);
+        gotomenu.setOnClickListener(this);
+        ImageView gotoadd = (ImageView) findViewById(R.id.addDoxod);
+        gotoadd.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.menugo:
+                intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.addDoxod:
+                intent = new Intent (MainActivity2.this, MainActivity4.class);
+                startActivity(intent);
+
+        }
+    }
+}
---
+++ Kurs/app/src/main/java/com/example/kurs/MainActivity3.java
@@ -0,0 +1,38 @@
+package com.example.kurs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity3 extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        ImageView gotomenu2 = (ImageView) findViewById(R.id.menugo1);
+        gotomenu2.setOnClickListener(this);
+        ImageView gotoadd1 = (ImageView) findViewById(R.id.addDoxod1);
+        gotoadd1.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.menugo1:
+                intent = new Intent(MainActivity3.this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.addDoxod1:
+                intent = new Intent(MainActivity3.this, MainActivity5.class);
+                startActivity(intent);
+
+        }
+    }
+}
